872910fbfd433b59ef4735cedbcda4fbcc62c94f
Nikita Ustinov <122388971+NikitaUstinov092@users.noreply.github.com>
2024-03-04 21:01:15 +0300

    Рефакторинг

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,24 +4,8 @@ using UnityEngine.AI;
 
 public sealed class Character : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed = 5.0f;
-
-    [ShowInInspector, ReadOnly]
-    private bool moveRequired;
-
-    [ShowInInspector, ReadOnly]
-    private Vector3 moveDirection;
-
     [SerializeField] 
     private NavMeshAgent _navMeshAgent;
-    
-    [Button]
-    public void Move(Vector3 direction)
-    {
-        moveRequired = true;
-        moveDirection = direction;
-    }
     
     [Button]
     public void MoveNavmesh(Transform direction)
